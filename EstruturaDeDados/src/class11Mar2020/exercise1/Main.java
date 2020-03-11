986399e9c9f976f2ab3fe6428d1fb63de7afd34b
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2020-03-11 09:00:33 -0300

    fix(Test): add test folder java

---
+++ EstruturaDeDados/src/class11Mar2020/exercise1/Main.java
@@ -0,0 +1,14 @@
+package class11Mar2020.exercise1;
+
+/*
+ * @author: rhian.costa
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("test");
+	}
+
+}
