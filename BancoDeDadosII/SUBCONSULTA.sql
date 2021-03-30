ad2154ee62d910193cacb4679c10278d30d63373
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2021-03-29 21:27:41 -0300

    feat: list 1 num 6

--- BancoDeDadosII/SUBCONSULTA.sql
+++ BancoDeDadosII/SUBCONSULTA.sql
@@ -7,3 +7,20 @@ FROM pedido p
 WHERE p.CodCliente IN 
 (SELECT c.CodCliente FROM cliente c WHERE c.Uf IN ('SC', 'RS'));
 
+-- 6 Crie uma consulta que exiba o código do cliente, o nome do cliente
+-- e o número dos seus pedidos ordenados pelo nome e posteriormente pelo
+-- código do pedido. Somente devem ser exibidos os pedidos dos vendedores
+-- que possuem a faixa de comissão “A”. Linhas: 1749
+
+SELECT 
+	p.CodCliente,
+	c.Nome,
+	p.CodPedido
+FROM pedido p 
+LEFT JOIN cliente c 
+	ON p.CodCliente = c.CodCliente 
+WHERE p.CodVendedor IN (SELECT v.CodVendedor FROM vendedor v WHERE v.FaixaComissao = 'A')
+GROUP BY p.CodPedido
+ORDER BY c.Nome, p.CodPedido;
+
+
