114e9b3263b2fa75cc2c3f6019f3fa78fc25defe
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2021-03-29 21:06:01 -0300

    feat: list 1 num 3

--- BancoDeDadosII/SUBCONSULTA.sql
+++ BancoDeDadosII/SUBCONSULTA.sql
@@ -1,5 +1,9 @@
 
+-- 3 Mostre a quantidade de pedidos dos clientes que moram no RS ou em SC. Linhas: 1
 
+SELECT 
+	COUNT(p.CodPedido)
+FROM pedido p 
+WHERE p.CodCliente IN 
+(SELECT c.CodCliente FROM cliente c WHERE c.Uf IN ('SC', 'RS'));
 
-
-assa
