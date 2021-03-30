48d213ba37d6d88020a6800d66a7be5dc105c62b
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2021-03-29 23:37:04 -0300

    feat: list 1 num 12

--- BancoDeDadosII/SUBCONSULTA.sql
+++ BancoDeDadosII/SUBCONSULTA.sql
@@ -1,5 +1,5 @@
 
--- 3 Mostre a quantidade de pedidos dos clientes que moram no RS ou em SC. Linhas: 1
+-- L1 EX3 Mostre a quantidade de pedidos dos clientes que moram no RS ou em SC. Linhas: 1
 
 SELECT 
 	COUNT(p.CodPedido)
@@ -7,7 +7,7 @@ FROM pedido p
 WHERE p.CodCliente IN 
 (SELECT c.CodCliente FROM cliente c WHERE c.Uf IN ('SC', 'RS'));
 
--- 6 Crie uma consulta que exiba o código do cliente, o nome do cliente
+-- L1 EX6 Crie uma consulta que exiba o código do cliente, o nome do cliente
 -- e o número dos seus pedidos ordenados pelo nome e posteriormente pelo
 -- código do pedido. Somente devem ser exibidos os pedidos dos vendedores
 -- que possuem a faixa de comissão “A”. Linhas: 1749
@@ -21,9 +21,9 @@ LEFT JOIN cliente c
 	ON p.CodCliente = c.CodCliente 
 WHERE p.CodVendedor IN (SELECT v.CodVendedor FROM vendedor v WHERE v.FaixaComissao = 'A')
 GROUP BY p.CodPedido
-ORDER BY p.CodCliente, c.Nome, p.CodPedido;
+ORDER BY c.Nome, p.CodPedido;
 
--- 9 Crie uma consulta que exiba o código do pedido e o somatório da quantidade
+-- L1 EX9 Crie uma consulta que exiba o código do pedido e o somatório da quantidade
 -- de itens desse pedido. Devem ser exibidos somente os pedidos em que o somatório
 -- das quantidades de itens de um pedido seja maior que a média da quantidade de
 -- itens de todos os pedidos. Linhas: 2508
@@ -46,6 +46,24 @@ WHERE p.Soma > (
 	 GROUP BY ip.CodPedido) AS p
 )
 GROUP BY p.CodPedido;
+
+
+-- L1 EX12 Selecione o código do produto, o nome do produto e o valor unitário dos
+-- produtos que possuam pelo menos um pedido com mais de 9 itens em sua quantidade.
+-- A lista deve ser ordenada pelo valor unitário em ordem decrescente. Linhas: 3063
+
+SELECT 
+	pr.*
+FROM produto pr
+INNER JOIN (
+	SELECT 
+		ip.CodProduto,
+		COALESCE(MAX(ip.Quantidade), 0) as QuantidadeMaxima
+	FROM itempedido ip
+	GROUP BY ip.CodProduto) AS p
+ON pr.CodProduto = p.CodProduto
+WHERE p.QuantidadeMaxima > 9
+ORDER BY pr.ValorUnitario DESC;
 
 
 
