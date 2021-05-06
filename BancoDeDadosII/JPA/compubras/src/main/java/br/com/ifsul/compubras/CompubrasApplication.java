a4b91a3b093faf1cd0dd653c8afdf017b8a30312
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2021-05-05 21:20:26 -0300

    feat: introducing inital project JPA

---
+++ BancoDeDadosII/JPA/compubras/src/main/java/br/com/ifsul/compubras/CompubrasApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifsul.compubras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompubrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompubrasApplication.class, args);
+	}
+
+}
---
+++ BancoDeDadosII/JPA/compubras/src/test/java/br/com/ifsul/compubras/CompubrasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifsul.compubras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompubrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
