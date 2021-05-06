07687849b0cd2a7b605ff5cec118146ee614c8c2
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2021-05-05 21:46:25 -0300

    feat: introducing initial mapping to entity

--- BancoDeDadosII/JPA/compubras/src/main/java/br/com/ifsul/compubras/CompubrasApplication.java
+++ BancoDeDadosII/JPA/compubras/src/main/java/br/com/ifsul/compubras/CompubrasApplication.java
@@ -3,6 +3,9 @@ package br.com.ifsul.compubras;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class CompubrasApplication {
 
@@ -10,4 +13,8 @@ public class CompubrasApplication {
 		SpringApplication.run(CompubrasApplication.class, args);
 	}
 
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Belem"));
+	}
 }
---
+++ BancoDeDadosII/JPA/compubras/src/main/java/br/com/ifsul/compubras/model/Cliente.java
@@ -0,0 +1,112 @@
+package br.com.ifsul.compubras.model;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table
+@Entity
+public class Cliente {
+
+    @Id
+    @Column(name = "CodCliente")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Basic(optional = false)
+    @Column(name = "Nome")
+    private String nome;
+
+    @Basic(optional = false)
+    @Column(name = "Endereco")
+    private String endereco;
+
+    @Basic(optional = false)
+    @Column(name = "Cidade")
+    private String cidade;
+
+    @Basic(optional = false)
+    @Column(name = "Cep")
+    private String cep;
+
+    @Basic(optional = false)
+    @Column(name = "Uf")
+    private String uf;
+
+    @Basic(optional = false)
+    @Column(name = "Ie")
+    private String ie;
+
+    public Cliente(Long id, String nome, String endereco, String cidade, String cep, String uf, String ie) {
+        this.id = id;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.cidade = cidade;
+        this.cep = cep;
+        this.uf = uf;
+        this.ie = ie;
+    }
+
+    public Cliente() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getIe() {
+        return ie;
+    }
+
+    public void setIe(String ie) {
+        this.ie = ie;
+    }
+}
